32a97200b2cf83ac3b1512b9f2f3c2777d4e0628
SaFrMo <sandermoolin@gmail.com>
2016-06-22 11:15:19 -0400

    Removed `_` from names, grammar and style edits

--- jquery.fitToParent.js
+++ jquery.fitToParent.js
@@ -5,7 +5,7 @@ jQuery.fn.fitToParent = function (options) {
 		// Cache the resize element
         var $el = jQuery(this);
         
-        // Get size parent (box to fit element in)
+        	// Get size parent (box to fit element in)
         var $box;
         if( $el.closest('.size-parent').length ) {
 	        $box = $el.closest('.size-parent');
@@ -15,20 +15,20 @@ jQuery.fn.fitToParent = function (options) {
 
 		// These are the defaults.
 		var settings = jQuery.extend({	
-				height_offset: 0,
-				width_offset: 0,
-				box_height: $box.height(),
-				box_width: $box.width(),
+				heightOffset: 0,
+				widthOffset: 0,
+				boxHeight: $box.height(),
+				boxWidth: $box.width(),
 				callback: null
 		}, options );
 		
 		// Setup box and element widths
         var width = $el.width();
         var height = $el.height();
-        var parentWidth = settings.box_width - settings.width_offset;
-        var parentHeight = settings.box_height - settings.height_offset;
+        var parentWidth = settings.boxWidth - settings.widthOffset;
+        var parentHeight = settings.boxHeight - settings.heightOffset;
 		
-		// Maintin aspect ratio
+		// Maintain aspect ratio
         var aspect = width / height;
         var parentAspect = parentWidth / parentHeight;
 		
@@ -43,12 +43,12 @@ jQuery.fn.fitToParent = function (options) {
 		
 		// Set new size of element
         $el.width(newWidth);
-		$el.height(newHeight); 
-		
-		// Fire callback
-		if (typeof(settings.callback) == "function") {
-			settings.callback(newWidth, newHeight);
-		}
+	$el.height(newHeight); 
+	
+	// Fire callback
+	if (typeof(settings.callback) == "function") {
+		settings.callback(newWidth, newHeight);
+	}
 
     });
 };
